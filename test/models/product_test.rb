5b4d1f6005b993ecf6d19e7376533e8c2e9f0846
Victor Kinelev <victor.kinelev.1@gmail.com>
2017-09-28 09:37:13 +0300

    Run the same tests multiple times

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,24 +1,30 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # self.pre_loaded_fixtures = false
-
-  def self.runnable_methods
-    Array.new(1000) { |i| "hey_man_#{i}".to_sym }
-  end
-
-  def method_missing(symbol)
-    if symbol.id2name.start_with?("hey_man")
-      hey_man
-    else
-      super
-    end
-  end
-
-  def hey_man
+  def test_discounters
     Product.create! title: 'Pink Hat', price: 1500
     Product.create! title: 'Dark Hat', price: 9900
 
-    assert_equal 1, Product.discounters.count
+    assert_equal 0, Product.discounters.count
+  end
+
+  # Make MiniTest think that we have X number of tests instead of one in this class
+  #
+  # Default implementation returns all methods starting with `test_`
+  # in the ProductTest class. MiniTests does not check a presence of those methods
+  # on this step, thus we can override it and return an array with any number
+  # of symbols. Symbols in the array have a predifiend format and this rule
+  # is used to handle a call to the fake methods with a real `test_discounters`
+  # with a help of `method_missing` method.
+  def self.runnable_methods
+    Array.new(500) { |i| "test_discounters_#{i}".to_sym }
+  end
+
+  def method_missing(symbol)
+    if symbol.id2name.start_with?("test_discounters")
+      test_discounters
+    else
+      super
+    end
   end
 end
